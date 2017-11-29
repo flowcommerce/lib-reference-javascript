04815bf59109ffdc0e8edac24fec4d9f82abbee9
Chris Hicks <chicks@flow.io>
2017-11-29 16:53:46 -0500

    format: add support for varying currency symbols and format options

--- src/format.js
+++ src/format.js
@@ -1,20 +1,61 @@
 /* eslint-disable import/prefer-default-export */
-import currencyFormat from '../data/currency-format.json';
+import currencyFormat from '../data/currency-format.v2.json';
 import accounting from './vendor/accounting';
+
+const formatters = {
+  strip_trailing_zeros: (value) => {
+    if (value && value.endsWith('.00')) {
+      return value.substring(0, value.length - 3);
+    }
+
+    return value;
+  },
+};
+
+function getFormatterFunctions(labelFormatters = []) {
+  const funcs = [];
+
+  labelFormatters.forEach((formatterKey) => {
+    const func = formatters[formatterKey];
+    if (func) {
+      funcs.push(func);
+    }
+  });
+
+  return funcs;
+}
+
+function createAccountingConfig(locale, options) {
+  const formatConfig = currencyFormat[locale];
+
+  if (!formatConfig) {
+    return undefined;
+  }
+
+  const symbolOpt = formatConfig.symbol[options.symbol] || formatConfig.symbol.primary;
+
+  return {
+    ...formatConfig,
+    symbol: symbolOpt,
+  };
+}
 
 /**
  * Localize amount based on locale
  *
  * Will return `amount` back if no format could be applied.
  *
+ * TODO: document options
+ *
  * @param  {number} amount  value of money (not in cents)
  * @param  {string} locale The locale in which to localize amount
  */
-export function currency(amount, locale) {
-  const accountingConfig = currencyFormat[locale];
-
+export function currency(amount, locale, options = {}) {
+  const accountingConfig = createAccountingConfig(locale, options);
   if (accountingConfig) {
-    return accounting.formatMoney(amount, accountingConfig);
+    const formatFunctions = getFormatterFunctions(options.label_formatters);
+    return formatFunctions.reduce((result, func) =>
+      func(result), accounting.formatMoney(amount, accountingConfig));
   }
 
   return amount;
---
+++ test/specs/format.js
@@ -0,0 +1,27 @@
+import { currency } from '../../src/format';
+
+describe('format', () => {
+  it('should format based on locale', () => {
+    const result = currency(1200.00, 'en-AU');
+    expect(result).to.equal('A$1,200.00');
+  });
+
+  it('should format based on locale and symbol narrow option', () => {
+    const result = currency(1200.00, 'en-AU', { symbol: 'narrow' });
+    expect(result).to.equal('$1,200.00');
+  });
+
+  it('should format based on locale and symbol primary option', () => {
+    const result = currency(1200.00, 'en-AU', { symbol: 'primary' });
+    expect(result).to.equal('A$1,200.00');
+  });
+
+  it('should format based on locale and label_formatters option', () => {
+    const options = {
+      symbol: 'narrow',
+      label_formatters: ['strip_trailing_zeros'],
+    };
+    const result = currency(1200.00, 'en-AU', options);
+    expect(result).to.equal('$1,200');
+  });
+});
