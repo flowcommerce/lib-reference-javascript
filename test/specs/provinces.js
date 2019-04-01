7ad500544966080bd0bbddd0e79ecf54452f753a
Christian Muñoz <ctumolosus@gmail.com>
2019-04-01 15:23:13 -0400

    Check provinces are top-level exports

--- test/specs/provinces.js
+++ test/specs/provinces.js
@@ -1,45 +1,50 @@
-import { all, findById, findByIso } from '../../src/provinces';
+import * as provinces from '../../src/provinces';
+import * as reference from '../../src';
 
 describe('provinces', () => {
   it('should export all provinces', () => {
-    expect(all).to.be.an('array').that.is.not.empty;
+    expect(provinces.all).to.be.an('array').that.is.not.empty;
+  });
+
+  it('should be top-level export', () => {
+    expect(reference).to.have.property('provinces', provinces);
   });
 
   context('findById', () => {
     it('should find a province by identifier (case-insensitive)', () => {
-      const ontario = findById('CAN-ON');
+      const ontario = provinces.findById('CAN-ON');
       expect(ontario).to.exist;
       expect(ontario).to.have.property('name', 'Ontario');
-      expect(findById('can-on')).to.deep.equal(ontario);
-      expect(findById('cAn-oN')).to.deep.equal(ontario);
-      expect(findById('Can-oN')).to.deep.equal(ontario);
+      expect(provinces.findById('can-on')).to.deep.equal(ontario);
+      expect(provinces.findById('cAn-oN')).to.deep.equal(ontario);
+      expect(provinces.findById('Can-oN')).to.deep.equal(ontario);
     });
 
     it('should return undefined for unmatched identifiers', () => {
-      expect(findById('FAKE-FAKE')).to.be.undefined;
+      expect(provinces.findById('FAKE-FAKE')).to.be.undefined;
     });
 
     it('should return undefined for unespecified identifier', () => {
-      expect(findById()).to.be.undefined;
+      expect(provinces.findById()).to.be.undefined;
     });
   });
 
   context('findByIso', () => {
     it('should find a province by ISO 3166-2 code (case-insensitive)', () => {
-      const ontario = findByIso('ON');
+      const ontario = provinces.findByIso('ON');
       expect(ontario).to.exist;
       expect(ontario).to.have.property('name', 'Ontario');
-      expect(findByIso('on')).to.deep.equal(ontario);
-      expect(findByIso('On')).to.deep.equal(ontario);
-      expect(findByIso('oN')).to.deep.equal(ontario);
+      expect(provinces.findByIso('on')).to.deep.equal(ontario);
+      expect(provinces.findByIso('On')).to.deep.equal(ontario);
+      expect(provinces.findByIso('oN')).to.deep.equal(ontario);
     });
 
     it('should return undefined for unmatched ISO codes', () => {
-      expect(findByIso('FAKE-FAKE')).to.be.undefined;
+      expect(provinces.findByIso('FAKE-FAKE')).to.be.undefined;
     });
 
     it('should return undefined for unespecified ISO codes', () => {
-      expect(findByIso()).to.be.undefined;
+      expect(provinces.findByIso()).to.be.undefined;
     });
   });
 });
