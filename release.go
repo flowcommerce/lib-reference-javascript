b554a96565953f7967640d9555d8fcad56eac0f0
paololim <paolo@flow.io>
2021-03-23 16:16:41 -0400

    Switching master to main

--- release.go
+++ release.go
@@ -6,15 +6,15 @@ import (
 
 func main() {
 	executor := executor.Create("lib-reference-javascript")
-	executor = executor.Add("git checkout master")
-	executor = executor.Add("git pull origin master")
+	executor = executor.Add("git checkout main")
+	executor = executor.Add("git pull origin main")
 	executor = executor.Add("npm install")
 	executor = executor.Add("npm run lint")
 	executor = executor.Add("npm run build")
 	executor = executor.Add("git add data")
 	executor = executor.Add("git commit -m \"autocommit: version `sem-info tag next`\" --allow-empty")
 	executor = executor.Add("dev tag --label micro")
-	executor = executor.Add("git push origin master")
+	executor = executor.Add("git push origin main")
 	executor = executor.Add("npm publish")
 	executor.Run()
 }
